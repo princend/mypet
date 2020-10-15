64ee4adecd941ab8afd0f3a36f867bf0d291f34f
Princend <waxdji3g4y3c04@gmail.com>
2020-10-16 00:43:14 +0800

    fill dialog

--- lib/main.dart
+++ lib/main.dart
@@ -25,14 +25,47 @@ class MyPet extends StatelessWidget {
   final String title;
 
   Widget _dialogBuilder(BuildContext context, Pet pet) {
-    return SimpleDialog(
+    ThemeData localTheme = Theme.of(context);
+    var simpleDialog = SimpleDialog(
+      contentPadding: EdgeInsets.zero,
       children: [
-        Container(
-          width: 80.0,
-          height: 120.0,
+        Image.network(pet.imageUrl, fit: BoxFit.fill),
+        Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                pet.name,
+                style: TextStyle(color: Colors.brown, fontSize: 26.0),
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                '性別：${pet.gender}',
+                style: localTheme.textTheme.subtitle1
+                    .copyWith(fontStyle: FontStyle.italic),
+              ),
+              Row(
+                children: [
+                  Text(
+                    '毛色：',
+                    style: localTheme.textTheme.subtitle1
+                        .copyWith(fontStyle: FontStyle.italic),
+                  ),
+                  Container(color: pet.color, height: 15, width: 15)
+                ],
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                pet.description,
+                style: localTheme.textTheme.bodyText1,
+              ),
+            ],
+          ),
         )
       ],
     );
+    return simpleDialog;
   }
 
   Widget _listItemBuilder(BuildContext context, int index) {
@@ -84,20 +117,23 @@ class Pet {
 final List<Pet> pets = <Pet>[
   Pet(
       name: 'NiChan',
-      description: '一支很可愛的柴犬',
+      description: '一支很可愛的柴犬，超級乖，不太會亂叫發病',
       gender: '母',
-      color: Colors.brown,
-      imageUrl: ''),
+      color: Color.fromRGBO(178, 128, 90, 1),
+      imageUrl:
+          'https://media.discordapp.net/attachments/721282789361713155/766328033174618112/IMG_20200730_073620.jpg?width=622&height=467'),
   Pet(
       name: '麻吉',
-      description: '一隻壞卯咪',
+      description: '一隻壞卯咪，每次都會搶亮亮的罐頭，並且發出嘶嘶聲',
       gender: '公',
-      color: Colors.black,
-      imageUrl: ''),
+      color: Color.fromRGBO(54, 50, 40, 1),
+      imageUrl:
+          'https://cdn.discordapp.com/attachments/721282789361713155/766328353501478982/Screenshot_20201015-235458.png'),
   Pet(
       name: '亮亮',
-      description: '三腳貓',
+      description: '三腳貓，年幼時被捕鼠夾夾斷了左前腿',
       gender: '公',
-      color: Colors.amber,
-      imageUrl: ''),
+      color: Color.fromRGBO(186, 148, 111, 1),
+      imageUrl:
+          'https://cdn.discordapp.com/attachments/721282789361713155/766327831642767380/IMG_20200222_131900.jpg'),
 ];
