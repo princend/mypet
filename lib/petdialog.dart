f0ab55be57e8ee9a211f2f49c9e6ade655106b9c
Princend <waxdji3g4y3c04@gmail.com>
2020-10-16 01:41:01 +0800

    separate

---
+++ lib/petdialog.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:mypet/pet.model.dart';
+
+class Petdialog extends StatelessWidget {
+  Petdialog({Key key, this.pet}) : super(key: key);
+  final Pet pet;
+  @override
+  Widget build(BuildContext context) {
+    ThemeData localTheme = Theme.of(context);
+    return SimpleDialog(
+      contentPadding: EdgeInsets.zero,
+      children: [
+        Image.network(pet.imageUrl, fit: BoxFit.fill),
+        Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                pet.name,
+                style: TextStyle(color: Colors.brown, fontSize: 26.0),
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                '性別：${pet.gender}',
+                style: localTheme.textTheme.subtitle1
+                    .copyWith(fontStyle: FontStyle.italic),
+              ),
+              Row(
+                children: [
+                  Text(
+                    '毛色：',
+                    style: localTheme.textTheme.subtitle1
+                        .copyWith(fontStyle: FontStyle.italic),
+                  ),
+                  Container(color: pet.color, height: 15, width: 15)
+                ],
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                pet.description,
+                style: localTheme.textTheme.bodyText1,
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
